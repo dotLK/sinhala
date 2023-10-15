b9c9dac105d417a43f337199f364b26f597851de
Dot LK (Sasindu) <110289670+dotLK@users.noreply.github.com>
2023-10-15 11:41:05 -0700

    Add files via upload

---
+++ Sinhala/__init__.py
@@ -0,0 +1,28 @@
+import random
+from Datas import *
+
+
+def prastha_pirulu():
+    return random.choice(Prastha_Pirulu_List)
+
+
+def pp():
+    return random.choice(Prastha_Pirulu_List)
+
+
+
+def theravili():
+    return random.choice(theravili_List)
+
+
+def thun_theravili():
+    return random.choice(thun_theravili_List)
+
+
+
+def thun():
+    return random.choice(thun_theravili_List)
+
+
+
+print(theravili())
